f80829ba83d7d8b9fbf8737f8e84bf61b9e7ff6c
David R. Piegdon <dgit@piegdon.de>
2017-10-09 21:47:38 +0000

    initial checkin

---
+++ xEpoch/main.cpp
@@ -0,0 +1,26 @@
+
+#include "Windows.h"
+#include <cstdio>
+#include <ctime>
+#include <cstring>
+
+int main(int argc, char **argv)
+{
+	char output[64];
+	size_t len;
+	time_t epoch;
+
+	time(&epoch);
+
+	snprintf(output, sizeof(output), "%llx\n", epoch);
+	len = strlen(output) + 1;
+
+	HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, len);
+	memcpy(GlobalLock(hMem), output, len);
+	GlobalUnlock(hMem);
+	OpenClipboard(0);
+	EmptyClipboard();
+	SetClipboardData(CF_TEXT, hMem);
+	CloseClipboard();
+}
+
