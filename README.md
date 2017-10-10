55d4bc33da87f1e162489d314f2e7b66bedba8bd
David R. Piegdon <dgit@piegdon.de>
2017-10-10 12:26:19 +0200

    allow appending a variable suffix

--- README.md
+++ README.md
@@ -2,5 +2,10 @@ Windows Application to put the current Epoch timestamp
 as hexadecimal number into the copy&paste buffer.
 I.e. the seconds since 00:00 Jan 1st 1970 UTC will as a hex number.
 
+If you change the filename of the executable to contain "xEpoch@somestring.exe",
+the "somestring" will be appended to the hex number in this way:
+
+	056789abc-somestring
+
 Build it using C++ of Visual Studio Express 2017.
 
--- xEpoch/main.cpp
+++ xEpoch/main.cpp
@@ -6,16 +6,33 @@
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int cmdShow)
 {
-	char output[64];
+	char output[256];
 	size_t len;
 	time_t epoch;
+	HGLOBAL hMem;
+	char progname[256];
+	char *shortname;
 
 	time(&epoch);
 
-	snprintf(output, sizeof(output), "%llx\n", epoch);
+	GetModuleFileName(NULL, progname, sizeof(progname));
+	for (shortname = progname + strlen(progname); shortname > progname; --shortname) {
+		if ('.' == *shortname) {
+			*shortname = 0;
+		} else if ('@' == *shortname) {
+			shortname += 1;
+			break;
+		}
+	}
+	if (shortname != progname) {
+		snprintf(output, sizeof(output), "%09llx-%s", epoch, shortname);
+	} else {
+		snprintf(output, sizeof(output), "%09llx", epoch);
+	}
+
 	len = strlen(output) + 1;
 
-	HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, len);
+	hMem = GlobalAlloc(GMEM_MOVEABLE, len);
 	memcpy(GlobalLock(hMem), output, len);
 	GlobalUnlock(hMem);
 	OpenClipboard(0);
@@ -23,4 +40,3 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int 
 	SetClipboardData(CF_TEXT, hMem);
 	CloseClipboard();
 }
-
